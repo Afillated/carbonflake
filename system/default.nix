94962f20d2b5595b6fc0b4bf12b33f3a526b43a8
Afillated <71583904+Afillated@users.noreply.github.com>
2025-09-17 21:20:23 +0530

    ^ part 2

--- system/default.nix
+++ system/default.nix
@@ -10,13 +10,31 @@
 
     ../system/hyprland/hyprland.nix
     ../system/grub/grub.nix
-    ../system/quickshell/quickshell.nix
+    # ../system/quickshell/quickshell.nix
     ../system/hardware
     ../system/greetd/greetd.nix
     ../system/hjem/hjem.nix
     ../system/shell
   
   ];
+
+
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
   fonts.packages = with pkgs;
     [
       font-awesome
