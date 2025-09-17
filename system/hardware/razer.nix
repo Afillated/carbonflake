25214cefba84727459ddffcf737267f23d28c3da
Afillated <71583904+Afillated@users.noreply.github.com>
2025-09-17 21:19:56 +0530

    fix keyboard stuff and remove quickshell for the time being

--- system/hardware/razer.nix
+++ system/hardware/razer.nix
@@ -4,8 +4,10 @@
   inputs,
   ...
 }: {
+    hardware.openrazer.enable = true;
     environment.systemPackages = with pkgs; [
       polychromatic
       openrazer-daemon
     ];
+    users.users.carbon.extraGroups = [ "openrazer" ];
   }
