42a56e65cfee57e2ae9df47ef23e4b18683dfc62
Afillated <71583904+Afillated@users.noreply.github.com>
2025-11-07 18:17:24 +0530

    add plymouth theme, also vesktop theme broke

--- system/boot/plymouth.nix
+++ system/boot/plymouth.nix
@@ -8,8 +8,8 @@
   boot = {
     plymouth = {
       enable = true;
-      theme = "catppuccin-mocha";
-      themePackages = [ (pkgs.catppuccin-plymouth.override { variant = "mocha"; }) ];
+      theme = "nixos-bgrt";
+      themePackages = [ pkgs.nixos-bgrt-plymouth ];
     };
 
     initrd.kernelModules = [
@@ -17,6 +17,16 @@
       "nvidia_modeset"
       "nvidia_uvm"
       "nvidia_drm"
+    ];
+
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
     ];
   };
 }
