32077bb58fe6bca267e54d880caebb94b4fc956b
Afillated <71583904+Afillated@users.noreply.github.com>
2025-11-15 03:18:49 +0530

    flake update

--- system/hardware/razer.nix
+++ system/hardware/razer.nix
@@ -3,11 +3,13 @@
   pkgs,
   inputs,
   ...
-}: {
-    hardware.openrazer.enable = true;
-    environment.systemPackages = with pkgs; [
-      polychromatic
-      openrazer-daemon
-    ];
-    users.users.carbon.extraGroups = [ "openrazer" ];
-  }
+}:
+{
+  hardware.openrazer.enable = true;
+  environment.systemPackages = with pkgs; [
+    polychromatic
+    openrazer-daemon
+  ];
+  users.users.carbon.extraGroups = [ "openrazer" ];
+
+}
