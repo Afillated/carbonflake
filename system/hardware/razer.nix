a577441a091adbf23390f5b05a0fafd680ada8b3
Afillated <71583904+Afillated@users.noreply.github.com>
2025-11-20 22:03:53 +0530

    add keyd configuration, change hyprland conf for the new keyd config

--- system/hardware/razer.nix
+++ system/hardware/razer.nix
@@ -9,7 +9,30 @@
   environment.systemPackages = with pkgs; [
     polychromatic
     openrazer-daemon
+    keyd
   ];
-  users.users.carbon.extraGroups = [ "openrazer" ];
+  users.users.carbon.extraGroups = [
+    "openrazer"
+    "keyd"
+  ];
 
+  services.keyd = {
+    enable = true;
+    keyboards = {
+      default = {
+        ids = [ "*" ];
+        settings = {
+          main = {
+            esc = "capslock";
+            capslock = "esc";
+            leftmeta = "leftalt";
+            leftalt = "leftmeta";
+
+          };
+          otherlayer = { };
+        };
+
+      };
+    };
+  };
 }
