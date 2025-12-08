b72d8718a22e2e17d02b30c47630158911b7bbd3
Afillated <71583904+Afillated@users.noreply.github.com>
2025-12-08 23:10:20 +0530

    add vencord(vesktop has discord call joining), add vlc for yazi opening

--- gaming/vesktop.nix
+++ gaming/vesktop.nix
@@ -7,6 +7,10 @@
 {
   environment.systemPackages = with pkgs; [
     vesktop
+    (discord.override {
+      # withOpenASAR = true; # can do this here too
+      withVencord = true;
+    })
   ];
 
   hjem.users.carbon.files = {
