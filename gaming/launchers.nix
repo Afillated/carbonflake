332964c6c9288d81b67d956d14d102cbef1e88e5
Afillated <71583904+Afillated@users.noreply.github.com>
2026-01-15 00:38:55 +0530

    add lutris

--- gaming/launchers.nix
+++ gaming/launchers.nix
@@ -2,5 +2,12 @@
 
   environment.systemPackages = with pkgs; [
     prismlauncher
+    (lutris.override {
+    # List of additional system libraries
+    extraLibraries = pkgs: [ ];
+
+    # List of additional system packages    
+    extraPkgs = pkgs: [ ];
+  })
   ];
 }
