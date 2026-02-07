cace4eb6360cb60c46aa096b6669fa462fa3dc45
Afillated <71583904+Afillated@users.noreply.github.com>
2026-02-07 23:32:08 +0530

    add dualsense fix

--- gaming/dualsense.nix
+++ gaming/dualsense.nix
@@ -2,19 +2,16 @@
 
   environment.systemPackages = with pkgs; [ dualsensectl ];
 
-  # services.udev.packages = [
-  #   (pkgs.writeTextFile {
-  #     name = "dualsense_udev";
-  #     text = ''
-  #         # Disable DualSense Touchpad acting as mouse
-  #         # USB
-  #         ATTRS{name}=="DualSense Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
-  #         # Bluetooth
-  #         ATTRS{name}=="Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
-  #       '';
-  #     destination = "/etc/udev/rules.d/50-dualsense-trackpad.rules";
-  #   })
-    
+  
+  environment.sessionVariables.ALSA_CONFIG_UCM2 = let
+    dualsense-ucm-conf = pkgs.alsa-ucm-conf.overrideAttrs (oldAttrs: {
+      patches =
+        (oldAttrs.patches or [])
+        ++ [
+          ../config/patches/dualsense-alsa-fix.patch
+        ];
+    });
+  in "${dualsense-ucm-conf}/share/alsa/ucm2";    
       
       
   
