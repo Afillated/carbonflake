e237d3a699528fee4b6cfaae9b00685f7faa11d3
Afillated <71583904+Afillated@users.noreply.github.com>
2026-02-08 01:16:47 +0530

    add fish for some shi

--- system/shell/default.nix
+++ system/shell/default.nix
@@ -33,16 +33,26 @@
     enableBashIntegration = true;
   };
 
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      set fish_greeting # Disable greeting
+    '';
+  };
+
   hjem.users.carbon = {
-    rum.programs.git.settings = {
-      user = {
-        email = "1583904+Afillated@users.noreply.github.com";
-        name = "Afillated";
+    rum.programs = {
+      git.settings = {
+        user = {
+          email = "1583904+Afillated@users.noreply.github.com";
+          name = "Afillated";
+        };
       };
     };
     files = {
       ".config/helix".source = ../../config/helix;
       ".config/fastfetch".source = ../../config/fastfetch;
     };
+
   };
 }
